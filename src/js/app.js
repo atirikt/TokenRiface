b970f5d4493e36bb8bed82297fbc8ecb827059f8
Anupam Kumar <atiriktmail@gmail.com>
2021-06-02 18:28:19 +0530

    add site info and placeholders for web3 insertions.
    Signed-off-by: Anupam Kumar <atiriktmail@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 console.log("The App is loaded");
+
+App = {
+  init:function(){
+    console.log("App initialized");
+  }
+}
+
+$(function() {
+  $(window).on('load', function(){
+    App.init();
+  })
+});
