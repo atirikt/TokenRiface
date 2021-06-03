29766d958700ec97619a853cb72e1d73cdf427da
Anupam Kumar <atiriktmail@gmail.com>
2021-06-03 18:48:19 +0530

    Interface setup= window.ethereum and TruffleContract addition.
    Signed-off-by: Anupam Kumar <atiriktmail@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,51 @@
 console.log("The App is loaded");
 
 App = {
+  //metamask does not apply web3 anymore, apply ethereum instead
+  web3Provider: null,
+  contracts: [],
+  account:'0x0',
+
   init:function(){
     console.log("App initialized");
+    return App.initWeb3();
+  },
+  initWeb3: function(){
+    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    return App.initContracts();
+  },
+  initContracts:function(){
+    $.getJSON("SellTokenR.json", function(sellTokenR){
+      App.contracts.SellTokenR = TruffleContract(sellTokenR);
+      App.contracts.SellTokenR.setProvider(App.web3Provider);
+      App.contracts.SellTokenR.deployed().then(function(sellTokenR){
+        console.log('sell token R address:',sellTokenR.address);
+      })
+    }).done(function(){
+        $.getJSON("TokenR.json", function(tokenR){
+          App.contracts.TokenR = TruffleContract(tokenR);
+          App.contracts.TokenR.setProvider(App.web3Provider);
+          App.contracts.TokenR.deployed().then(function(tokenR){
+            console.log('Token R address:',tokenR.address);
+          });
+        }).done(function(){
+          App.render();
+        })
+      });
+    //check accounts.
+    ethereum.request({ method: 'eth_accounts' }).then((result)=>{
+      console.log(result);
+    }).catch((error)=>{
+      console.log(error);
+    });
+  },
+
+  render: async function(){
+    //load account data
+    await window.ethereum.enable();
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    console.log(accounts);
+    $("#accountAddress").html("a/c(s):  " + accounts);
   }
 }
 
